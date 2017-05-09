b5d645162b3f6210add48c83dc80db0958465e17
delightful-Kdub <krysta@SchmutzigKillahKdub.fios-router.home>
2017-05-09 16:23:49 -0400

    final update

--- js/main.js
+++ js/main.js
@@ -5,7 +5,22 @@ jQuery(function($) {
     var label_id = $(this).attr('id');
     $('.tab-content').removeClass('open');
     $('.'+label_id+'-content').addClass('open');
+    $('.menu label').removeClass('activetab');
+    $('.menu label').addClass('tab');
+    $(this).removeClass('tab');
+    $(this).addClass('activetab');
   });
-});
 
-//trying to get the active tab to follow my visual designs and indicate its active
+  //trying to get the active tab to follow my visual designs and indicate its active
+
+
+
+  //instead of below, do this:  https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_textarea_value
+
+  $('input#goToTree').click(function() {
+    var text = $('textarea .notesTextArea').val();
+    //send to server and process response
+    console.log(text);
+  });
+
+});
